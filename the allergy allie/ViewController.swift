b1f85c4221d6a9f74326ef70f27589295e6fb684
Jacob Kantor <jacobmk8@gmail.com>
2017-10-27 23:44:28 -0400

    First Commit

--- the allergy allie/ViewController.swift
+++ the allergy allie/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
-
-
+    // Above this is the xcode bullshit ignore
+    //This is the link needed to acess the google form.
+    
+    
 }
 
